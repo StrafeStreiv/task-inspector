1d109d57b60925626d84bf19d088921521b5c15e
Aleksandr Porfirev <2002wasj@mail.ru>
2026-03-14 15:30:41 +0300

    Add DbContext and SQLite configuration

---
+++ TaskInspector/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskInspector.Models;
+
+namespace TaskInspector.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<TaskItem> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<TaskItem>()
+                .HasIndex(t => t.Status);
+
+            modelBuilder.Entity<TaskItem>()
+                .HasIndex(t => t.Assignee);
+
+            modelBuilder.Entity<TaskItem>()
+                .Property(t => t.Title)
+                .IsRequired()
+                .HasMaxLength(200);
+        }
+    }
+}
